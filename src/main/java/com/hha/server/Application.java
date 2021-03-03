d5d341264c4dfb27f819c1d2912ba461e6cad527
Rakim Zubair <rakimz@sfu.ca>
2021-03-03 01:42:05 -0800

    updating db

--- src/main/java/com/hha/server/Application.java
+++ src/main/java/com/hha/server/Application.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 
@@ -90,6 +91,7 @@ public class Application implements CommandLineRunner {
 
 }
 
+@Repository
 interface ClientRepository extends JpaRepository<Client, Long> {
 
 }
