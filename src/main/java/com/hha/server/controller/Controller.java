98ad66ae9d09278124531bd4375683bb1e42a6b1
Rakim Zubair <rakimz@sfu.ca>
2021-03-03 17:07:38 -0800

    test

--- src/main/java/com/hha/server/controller/Controller.java
+++ src/main/java/com/hha/server/controller/Controller.java
@@ -3,7 +3,6 @@ package com.hha.server.controller;
 import com.hha.server.model.Client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +12,7 @@ import java.util.List;
 
 @RestController
 public class Controller {
+    @Autowired
     private final ClientRepository clientRepository;
 
     public Controller(ClientRepository clientRepository) {
@@ -39,6 +39,7 @@ public class Controller {
     }
 }
 
+@Repository
 interface ClientRepository extends JpaRepository<Client, Long> {
 
 }
